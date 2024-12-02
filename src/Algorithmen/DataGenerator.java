62f969012c8ddc7172abc3ff475b5bfcff072af4
HannesNeyer <hannes.neyer@student.htldornbirn.at>
2024-12-02 10:58:51 +0100

    Finished DataGenerator

--- src/Algorithmen/DataGenerator.java
+++ src/Algorithmen/DataGenerator.java
@@ -3,14 +3,14 @@ import java.util.Random;
 
 public class DataGenerator {
     public static int[] generateDataArray(int size) {
-        int[] arr = new int[5];
+        int[] arr = new int[size];
         Random rand = new Random();
 
         for (int i = 0; i < 5; i++) {
             int number = rand.nextInt();
             arr[i] = number;
+            System.out.println(arr[i]);
         }
-
         return arr;
     }
 }
--- src/Algorithmen/main.java
+++ src/Algorithmen/main.java
@@ -1,5 +1,8 @@
 package Algorithmen;
 
-
-    
-
+public class main {
+    public static void main(String[] args) {
+        DataGenerator DG = new DataGenerator();
+        DG.generateDataArray(5);
+    }
+}
